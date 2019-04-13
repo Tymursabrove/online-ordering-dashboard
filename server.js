f8e07272000ddfdb3ce077d426e6b23fac899b67
Tymursabrove <tymursabrove0329@gmail.com>
2019-04-13 14:03:50 +0100

    1st commit

---
+++ server.js
@@ -0,0 +1,56 @@
+// set up ===============================================================
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+var cors = require('cors');
+require('dotenv').config()
+
+// DB configuration ===============================================================
+const dbRoute = process.env.DB_URI;
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+// router files ===============================================================
+var testRoutes   = require('./routes/test');
+
+// routes ======================================================================
+app.use('/test', testRoutes);
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'client/public/index.html'));
+})
+
+//start server
+app.listen(port, (req, res) => {
+  console.log( `server listening on port: ${port}`);
+})
